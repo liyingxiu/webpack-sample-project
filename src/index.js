8dc320ac942008273c76ff4912c298a449d427f9
liyingxiu <yingxiuim@gmail.com>
2020-03-02 19:41:32 +0800

    资源管理

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
+
+function component() {
+  let element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // 将图像添加到我们已经存在的 div 中
+  let myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
