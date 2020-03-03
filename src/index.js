e9c4adef3919e5d0c0e551fa4224b3cf9965d37e
liyingxiu <yingxiuim@gmail.com>
2020-03-03 09:56:01 +0800

    热替换

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
+import printMe from './print.js';
+import './styles.css';
 
 function component() {
   let element = document.createElement('div');
+  var btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // 将图像添加到我们已经存在的 div 中
-  let myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = '点击这里，然后查看 console！';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
+  element.appendChild(btn);
 
   return element;
 }
 
 document.body.appendChild(component());
+let element = component(); // 存储 element，以在 print.js 修改时重新渲染
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    element = component(); // 重新渲染 "component"，以便更新 click 事件处理函数
+    document.body.appendChild(element);
+  })
+}
